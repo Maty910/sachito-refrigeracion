529a16b634f68d3d746e2af50abb30a218d16be1
Maty910 <matychacong@gmail.com>
2025-12-29 20:18:19 -0300

    fix:  home page texts

--- apps/web/src/pages/HomePage.tsx
+++ apps/web/src/pages/HomePage.tsx
@@ -4,6 +4,7 @@ import { WhyUsGrid } from '../components/sections/WhyUsGrid';
 import { AboutSection } from '../components/sections/AboutSection';
 import { GuaranteeSection } from '../components/sections/GuaranteeSection';
 import { Testimonials } from '../components/sections/Testimonials'; // <--- Importar
+import { ServicesList } from '../components/sections/ServicesList';
 
 export const HomePage = () => {
   return (
@@ -11,6 +12,7 @@ export const HomePage = () => {
       <Hero />             {/* Oscuro */}
       <AboutSection />     {/* Claro (Ice) */}
       <HomeServices />     {/* Oscuro */}
+      <ServicesList />
       <WhyUsGrid />        {/* Claro (Ice) */}
       <GuaranteeSection /> {/* Oscuro */}
       <Testimonials />     {/* Claro (Ice) */}
